cf1e79cb2c9ff5f747c784399809a45a380a71d8
Faithvorite <57932015+Faithvorite@users.noreply.github.com>
2023-05-11 14:27:03 -0500

    navlinks test

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -9,7 +9,39 @@ test("Open home page and verify title", async ({page}) => {
 test("Verify logo is visible and has alt title", async ({page}) => {
     await page.goto("https://www.fifa.com/fifaplus/en/home");
     
-    const logo = page.locator('[href="/fifaplus/en/home] img');
+    const logo = page.locator('[href="https://www.fifa.com/fifaplus/en/home?intcmp=(p_fifaplus)_(c_webheader-main)_(sc_logo)_(ssc_)_(da_04052023)_(l_en)"] img');
     await expect(logo).toBeVisible();
-    await expect(logo).toHaveAttribute('title', 'Qatar 2022')
+    await expect(logo).toHaveAttribute('title', 'FIFA Plus Logo')
+})
+
+test("Verify navigation links", async ({page}) => {
+    const expectedLinks = [
+     "FIFA U-20 WORLD CUP 2023™",
+     "FIFA WOMEN'S WORLD CUP 2023™",
+     "FIFA BEACH SOCCER WORLD CUP 2023™",
+      "OTHER FIFA TOURNAMENTS",
+      "FIFA WORLD CUP 2026™",
+      "ORIGINALS",
+      "ARCHIVE",
+      "MATCH CENTRE",
+       "LIVE STREAMING",
+      "HIGHLIGHTS",
+      "PLAY ZONE",
+      "FIVES",
+      "GLOBAL GOALSCORER",
+      "WHO AM I?",
+       "HEAD TO HEAD",
+      "TRIVIA",
+      "FIFA STORE",
+      "BUY TICKETS",
+      "FIFA+ COLLECT",
+      "NEWS",
+    ];
+    await page.goto("https://www.fifa.com/fifaplus/en/home");
+    
+    const navLinks = page.locator('#mainLinksID a')
+
+    await page.waitForTimeout(3000);
+
+    expect( await navLinks.allInnerTexts()).toEqual(expectedLinks)
 })
